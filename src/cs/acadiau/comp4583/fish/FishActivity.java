56bec3c11d23bfc97f7c786bb351e42e5d136c20
Samuel Coleman <samuel@seenet.ca>
2014-02-19 15:25:51 -0400

    Basic application shell.

---
+++ src/cs/acadiau/comp4583/fish/FishActivity.java
@@ -0,0 +1,14 @@
+package cs.acadiau.comp4583.fish;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FishActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fish);
+    }
+}
