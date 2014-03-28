2479fdc307ba9337f2cc7a8c78d359c71b02c412
Samuel Coleman <samuel@seenet.ca>
2014-03-28 05:19:48 -0300

    Add login capability.

---
+++ src/ca/acadiau/cs/comp4583/fish/LoginActivity.java
@@ -0,0 +1,141 @@
+package ca.acadiau.cs.comp4583.fish;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import ca.acadiau.cs.comp4583.fish.data.User;
+import ca.acadiau.cs.comp4583.fish.data.persistence.LoginHandler;
+import ca.acadiau.cs.comp4583.fish.data.persistence.TrackMyFishLoginProvider;
+
+/**
+ * Interface for user logins.
+ * 
+ * @since 1.0.0
+ * @author Samuel Coleman <105709c@acadiau.ca>
+ */
+public class LoginActivity extends Activity implements LoginHandler
+{
+    public static final String LOGIN_PREFS = "TMFPreferences";
+    public static final String LOGIN_PREFS_USERNAME = "username";
+
+    private ProgressDialog loginDialog = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        /* Set the layout. */
+        this.setContentView(R.layout.login);
+
+        final Button backButton = (Button) this.findViewById(R.id.back_from_login_button);
+        final Button loginButton = (Button) this.findViewById(R.id.login_button);
+        final EditText usernameEditText = (EditText) this.findViewById(R.id.login_text_edit);
+        final EditText passwordEditText = (EditText) this.findViewById(R.id.pass_text_edit);
+
+        /* Just leave the activity when the user hits Back. */
+        backButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                LoginActivity.this.finish();
+            }
+        });
+
+        loginButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                LoginActivity.this.showLoginDialog();
+
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                TrackMyFishLoginProvider loginProvider = new TrackMyFishLoginProvider(
+                        getString(R.string.trackmyfish_auth_endpoint));
+                loginProvider.addLoginHandler(LoginActivity.this);
+                loginProvider.validate(username, password);
+            }
+        });
+
+        /* The Login button shouldn't be enabled until a username and password
+         * have been filled out. */
+        loginButton.setEnabled(false);
+
+        /* Validates the eligibility of the use of the login button. */
+        TextWatcher loginEligibilityWatcher = new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                loginButton.setEnabled(usernameEditText.getText().length() > 0
+                        && passwordEditText.getText().length() > 0);
+            }
+        };
+
+        /* If either the username or password field are blank, we want to
+         * disallow logging in. */
+        (usernameEditText).addTextChangedListener(loginEligibilityWatcher);
+        (passwordEditText).addTextChangedListener(loginEligibilityWatcher);
+    }
+
+    private void showLoginDialog()
+    {
+        this.loginDialog = ProgressDialog.show(this,
+                getString(R.string.login_dialog_title),
+                getString(R.string.login_dialog_message));
+    }
+
+    private void hideLoginDialog()
+    {
+        if (this.loginDialog != null)
+            this.loginDialog.dismiss();
+    }
+
+    @Override
+    public void onLoginSuccess(User user)
+    {
+        this.hideLoginDialog();
+
+        SharedPreferences preferences = getSharedPreferences(LoginActivity.LOGIN_PREFS, 0);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(LoginActivity.LOGIN_PREFS_USERNAME, user.getUsername());
+        editor.commit();
+
+        this.finish();
+    }
+
+    @Override
+    public void onLoginFailure(final int messageRes)
+    {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run()
+            {
+                LoginActivity.this.hideLoginDialog();
+
+                new AlertDialog.Builder(LoginActivity.this)
+                        .setMessage(getString(messageRes))
+                        .setPositiveButton(R.string.ok, null)
+                        .create()
+                        .show();
+            }
+        });
+    }
+}
--- src/ca/acadiau/cs/comp4583/fish/MainActivity.java
+++ src/ca/acadiau/cs/comp4583/fish/MainActivity.java
@@ -1,30 +1,34 @@
 package ca.acadiau.cs.comp4583.fish;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
-import android.widget.Button;
-
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button begin_new_session_button = (Button) findViewById(R.id.begin_session_button);
-        begin_new_session_button.setOnClickListener(new View.OnClickListener() {
+
+        ((Button) findViewById(R.id.begin_session_button)).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
             {
-                Intent i = new Intent(getApplicationContext(), NewSessionActivity.class);
-                startActivity(i);
+                startActivity(new Intent(MainActivity.this, NewSessionActivity.class));
             }
         });
 
+        ((Button) findViewById(R.id.login_button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
     }
 
     @Override
@@ -34,5 +38,4 @@ public class MainActivity extends Activity
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
 }
